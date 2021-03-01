9d2928195e03fb111eeed4c3c66fddd493861272
Jani Willman <jani@willman.dev>
2021-03-01 20:52:04 +0200

    Fixed test

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import ReactDOM from 'react-dom';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
