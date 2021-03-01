ee0901e0055c2ad5f4a223c0c5373019cc0e449d
Jani Willman <jani@willman.dev>
2021-03-01 20:46:29 +0200

    Sketching out the different components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import "./App.css";
 
 function App() {
+  var items = ["item 1", "item 2", "item 3"];
+
   return (
     <div className="App">
       <h1>Flickr search</h1>
-      <input id="search"></input>
+      <Search />
+      <Results items={items} />
     </div>
   );
+}
+
+function Search() {
+  return (
+    <input id="search"></input>
+  )
+}
+
+function Results(props) {
+  const resultItems = props.items.map((x) => <li>{x}</li>  )
+  return (
+    <div id="results">
+      <ul>
+        {resultItems}
+      </ul>
+    </div>
+  )
 }
 
 export default App;
