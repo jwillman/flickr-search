27d1b64c32cb8ebd8f9c9c4bb29fa1c0ad175288
Jani Willman <jani@willman.dev>
2021-03-02 20:25:06 +0200

    Search input now goes to the state

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [searchString, setSearchString] = useState("");
   var items = ["item 1", "item 2", "item 3"];
 
   return (
     <div className="App">
       <h1>Flickr search</h1>
-      <Search />
+      <Search setSearchString={setSearchString} />
+      search: {searchString}
       <Results items={items} />
     </div>
   );
 }
 
-function Search() {
+function Search(props) {
   return (
-    <input id="search"></input>
-  )
+    <input
+      id="search"
+      onChange={(e) => props.setSearchString(e.target.value)}
+    ></input>
+  );
 }
 
 function Results(props) {
-  const resultItems = props.items.map((x) => <li>{x}</li>  )
+  const resultItems = props.items.map((x, index) => <li key={index}>{x}</li>);
   return (
     <div id="results">
-      <ul>
-        {resultItems}
-      </ul>
+      <ul>{resultItems}</ul>
     </div>
-  )
+  );
 }
 
 export default App;
