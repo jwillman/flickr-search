9f841f6fce16125c8f04829c6dd4b94625319355
Jani Willman <jani@willman.dev>
2021-03-04 20:09:46 +0200

    Build broken, fix

--- src/flickr.js
+++ src/flickr.js
@@ -1,12 +1,11 @@
-const photosEndpoint =
-    "https://api.flickr.com/services/api/explore/flickr.photos.search";
+//const photosEndpoint = "https://api.flickr.com/services/api/explore/flickr.photos.search";
 
 // TODO stay under 3600 queries per hour
 // TODO cache results
 export function flickrSearch(searchstring) {
     console.log(`Flickr search called with ${searchstring}`);
 
-    //"https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=&text=flower&format=json&nojsoncallback=1";
+    //("https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=&text=flower&format=json&nojsoncallback=1");
 
     // const callFlickrApi = async () => {
     //   const response = await fetch(photosEndpoint, {
