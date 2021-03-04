5db3a6471821e689ba66d3fc7694237dc5fca9b9
Jani Willman <jani@willman.dev>
2021-03-04 20:05:41 +0200

    Added server.js to serve as backend for the Flickr queries from client/react

--- src/App.js
+++ src/App.js
@@ -3,35 +3,35 @@ import React, { useEffect, useState } from "react";
 import { flickrSearch } from "./flickr.js";
 
 function App() {
-  const [searchString, setSearchString] = useState("");
-  var items = ["item 1", "item 2", "item 3"];
+    const [searchString, setSearchString] = useState("");
+    var items = ["item 1", "item 2", "item 3"];
 
-  useEffect(() => {
-    flickrSearch(searchString);
-  });
+    useEffect(() => {
+        flickrSearch(searchString);
+    });
 
-  return (
-    <div className="App">
-      <h1>Flickr search</h1>
-      <Search setSearchString={setSearchString} />
-      <Results items={items} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Flickr search</h1>
+            <Search setSearchString={setSearchString} />
+            <Results items={items} />
+        </div>
+    );
 }
 
 function Search(props) {
-  return (
-    <input onChange={(e) => props.setSearchString(e.target.value)}></input>
-  );
+    return (
+        <input onChange={(e) => props.setSearchString(e.target.value)}></input>
+    );
 }
 
 function Results(props) {
-  const resultItems = props.items.map((x, index) => <li key={index}>{x}</li>);
-  return (
-    <div id="results">
-      <ul>{resultItems}</ul>
-    </div>
-  );
+    const resultItems = props.items.map((x, index) => <li key={index}>{x}</li>);
+    return (
+        <div id="results">
+            <ul>{resultItems}</ul>
+        </div>
+    );
 }
 
 export default App;
--- src/flickr.js
+++ src/flickr.js
@@ -1,22 +1,22 @@
 const photosEndpoint =
-  "https://api.flickr.com/services/api/explore/flickr.photos.search";
+    "https://api.flickr.com/services/api/explore/flickr.photos.search";
 
 // TODO stay under 3600 queries per hour
 // TODO cache results
 export function flickrSearch(searchstring) {
-  console.log(`Flickr search called with ${searchstring}`);
+    console.log(`Flickr search called with ${searchstring}`);
 
-  //"https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=&text=flower&format=json&nojsoncallback=1";
+    //"https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=&text=flower&format=json&nojsoncallback=1";
 
-  // const callFlickrApi = async () => {
-  //   const response = await fetch(photosEndpoint, {
-  //     method: "POST",
-  //     body: "myBody", // string or object
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   });
-  //   const myJson = await response.json(); //extract JSON from the http response
-  //   // do something with myJson
-  // };
+    // const callFlickrApi = async () => {
+    //   const response = await fetch(photosEndpoint, {
+    //     method: "POST",
+    //     body: "myBody", // string or object
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //   });
+    //   const myJson = await response.json(); //extract JSON from the http response
+    //   // do something with myJson
+    // };
 }
